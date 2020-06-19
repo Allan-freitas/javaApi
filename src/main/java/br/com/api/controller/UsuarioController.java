fc1db1a1a7b6988d13562cea97a27935ca7e23f7
Allan Freitas <freitas_allan@hotmail.com>
2020-06-19 11:39:17 -0300

    UsuarioController

--- src/main/java/br/com/api/controller/UsuarioController.java
+++ src/main/java/br/com/api/controller/UsuarioController.java
@@ -1,7 +1,13 @@
 package br.com.api.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UsuarioController {
+
+    @RequestMapping({ "/hello" })
+    public String firstPage() {
+        return "Hello there!";
+    }
 }
